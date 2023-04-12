d8eeac64c0d10b73f69925f3c854be70f1082c84
Noah Koo <sjdpclsrnrk@gmail.com>
2023-04-13 00:39:08 +0900

    feat(app): Try to make a Modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import A from "./A";
 import "./App.css";
 import { useState } from "react";
 
@@ -54,12 +55,17 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>4월 12일 발행</p>
       </div>
+      <Modal />
+    </div>
+  );
+}
 
-      <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
